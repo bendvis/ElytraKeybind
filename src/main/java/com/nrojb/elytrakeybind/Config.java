8f8b152d13eb972861b8a32f44495894f5aed094
BEN-LAPTOP\bendv <bendvis@gmail.com>
2026-08-12 13:46:18 -0700

    Add config and english strings

--- src/main/java/com/nrojb/elytrakeybind/Config.java
+++ src/main/java/com/nrojb/elytrakeybind/Config.java
@@ -9,5 +9,21 @@ public class Config {
             .comment("Toggle the \"Elytra Toggle: true\" messages")
             .define("disableChatMessages", false);
 
+    public static final ModConfigSpec.BooleanValue showEnabledIcon = BUILDER
+            .comment("Show an icon on the HUD when Elytra are enabled.")
+            .define("showEnabledIcon", true);
+
+    public static final ModConfigSpec.BooleanValue showDisabledIcon = BUILDER
+            .comment("Show an icon on the HUD when Elytra are disabled.")
+            .define("showDisabledIcon", true);
+
+    public static final ModConfigSpec.IntValue xOffset = BUILDER
+            .comment("X Position for the Enabled/Disabled icon.")
+            .defineInRange("iconXPosition", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+    public static final ModConfigSpec.IntValue yOffset = BUILDER
+            .comment("Y Position for the Enabled/Disabled icon.")
+            .defineInRange("iconYPosition", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
     static final ModConfigSpec SPEC = BUILDER.build();
 }
